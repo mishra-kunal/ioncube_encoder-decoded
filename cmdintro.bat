152c984668c6627da0b2321fb8dafa76047610f8
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2023-01-08 19:43:05 +0330

    Add files via upload

---
+++ cmdintro.bat
@@ -0,0 +1,21 @@
+@echo : ionCube PHP Encoder Command Window
+@echo :
+@echo : Encode scripts as follows:
+@echo :
+@echo :   For PHP 4 scripts use "ioncube_encoder.exe"
+@echo :   For PHP 5 scripts before PHP 5.3 use "ioncube_encoder5.exe"
+@echo :   For PHP 5.3+ scripts use "ioncube_encoder53.exe"
+@echo :   For PHP 7.1+ scripts use "ioncube_encoder71.exe"
+@echo :   For PHP 8.1+ scripts use "ioncube_encoder81.exe"
+@echo :
+@echo : followed by command line options.
+@echo :
+@echo : For example:
+@echo :
+@echo :  ioncube_encoder81.exe c:\myproject -o c:\myproject-encoded
+@echo :
+@echo : See the User Guide PDF for full command line details.
+@echo :
+@echo : Please contact us at https://support.ioncube.com for technical support during
+@echo : the evaluation period
+@set PATH=C:\Program Files (x86)\ionCube PHP Encoder 12.0.2 Evaluation;%PATH%
